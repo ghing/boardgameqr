e8675e730cd709e1c2a39568d58713a9a21cf86d
Geoffrey Hing <geoff@terrorware.com>
2011-12-26 01:17:09 -0600

    Added autocompleting search for game titles to generate code.

--- boardgameqr.coffee
+++ boardgameqr.coffee
@@ -1,23 +1,35 @@
 QRCode = require 'qrcode'
 Connect = require 'connect'
 Mu = require 'mu2'
+Request = require 'request'
+Parser = require 'xml2json'
 
 Mu.root = process.cwd() + '/templates'
 
 port = process.env.PORT || 8080
 
 Connect.createServer()
+.use('/', Connect.query())
 .use '/', Connect.router (app) ->
     app.get '/', (req, res, next) ->
-      # TODO: Show search form with autocomplete like
-      # http://www.boardgamegeek.com/xmlapi2/search?query=vivajava&type=boardgame
-      res.writeHead 200, {"Content-Type": "text/plain"}
-      res.end "Got here"
+      res.writeHead 200, {"Content-Type": "text/html"}
+      Mu.compile 'qrcode.html', (err, parsed) ->
+        if err
+          console.log 'Error compiling template'
+
+        view = { title: 'Board Game QR', has_qrcode: false }
+
+        buffer = ''
+        stream = Mu.render parsed, view
+
+        stream.on 'data', (c) -> buffer += c
+        stream.on 'end', () -> res.end buffer
+
     app.get '/game/:id', (req, res, next) ->
       geekURL = "http://boardgamegeek.com/boardgame/" + req.params.id + "/#collection"
       res.writeHead 302, {"Location": geekURL}
       res.end "Redirecting to Board Game Geek"
-    app.get '/game/:id/qr', (req, res, next) ->
+    app.get '/qr/game/:id', (req, res, next) ->
       gameURL = "http://" + req.headers.host + "/game/" + req.params.id + "/"
       res.writeHead 200, {'Content-Type': "text/html"}
       QRCode.toDataURL gameURL, (err, url) ->
@@ -25,12 +37,24 @@ Connect.createServer()
           if err
             console.log 'Error compiling template'
 
-          view = { title: 'Board Game QR', qrcode_url: url }
+          view = { title: 'Board Game QR', has_qrcode: true, qrcode_url: url }
 
           buffer = ''
           stream = Mu.render parsed, view
 
           stream.on 'data', (c) -> buffer += c
           stream.on 'end', () -> res.end buffer
+
+    app.get '/api/0.1/games/autocomplete', (req, res, next) ->
+      Request 'http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=' + req.query.term, (error, response, body) ->
+        if not error and response.statusCode == 200
+          js = Parser.toJson body, {object: true}
+          items = []
+          if js.items.item?
+            items = ({ label: item.name.value, value: item.id } for item in js.items.item)
+          
+          res.writeHead 200, {'Content-Type': 'application/json'}
+          res.end JSON.stringify(items)
+
 .use('/public', Connect.static __dirname + '/public')
 .listen port
--- boardgameqr.js
+++ boardgameqr.js
@@ -1,5 +1,5 @@
 (function() {
-  var Connect, Mu, QRCode, port;
+  var Connect, Mu, Parser, QRCode, Request, port;
 
   QRCode = require('qrcode');
 
@@ -7,16 +7,35 @@
 
   Mu = require('mu2');
 
+  Request = require('request');
+
+  Parser = require('xml2json');
+
   Mu.root = process.cwd() + '/templates';
 
   port = process.env.PORT || 8080;
 
-  Connect.createServer().use('/', Connect.router(function(app) {
+  Connect.createServer().use('/', Connect.query()).use('/', Connect.router(function(app) {
     app.get('/', function(req, res, next) {
       res.writeHead(200, {
-        "Content-Type": "text/plain"
+        "Content-Type": "text/html"
       });
-      return res.end("Got here");
+      return Mu.compile('qrcode.html', function(err, parsed) {
+        var buffer, stream, view;
+        if (err) console.log('Error compiling template');
+        view = {
+          title: 'Board Game QR',
+          has_qrcode: false
+        };
+        buffer = '';
+        stream = Mu.render(parsed, view);
+        stream.on('data', function(c) {
+          return buffer += c;
+        });
+        return stream.on('end', function() {
+          return res.end(buffer);
+        });
+      });
     });
     app.get('/game/:id', function(req, res, next) {
       var geekURL;
@@ -26,7 +45,7 @@
       });
       return res.end("Redirecting to Board Game Geek");
     });
-    return app.get('/game/:id/qr', function(req, res, next) {
+    app.get('/qr/game/:id', function(req, res, next) {
       var gameURL;
       gameURL = "http://" + req.headers.host + "/game/" + req.params.id + "/";
       res.writeHead(200, {
@@ -38,6 +57,7 @@
           if (err) console.log('Error compiling template');
           view = {
             title: 'Board Game QR',
+            has_qrcode: true,
             qrcode_url: url
           };
           buffer = '';
@@ -49,6 +69,36 @@
             return res.end(buffer);
           });
         });
+      });
+    });
+    return app.get('/api/0.1/games/autocomplete', function(req, res, next) {
+      return Request('http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=' + req.query.term, function(error, response, body) {
+        var item, items, js;
+        if (!error && response.statusCode === 200) {
+          js = Parser.toJson(body, {
+            object: true
+          });
+          items = [];
+          if (js.items.item != null) {
+            items = (function() {
+              var _i, _len, _ref, _results;
+              _ref = js.items.item;
+              _results = [];
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                item = _ref[_i];
+                _results.push({
+                  label: item.name.value,
+                  value: item.id
+                });
+              }
+              return _results;
+            })();
+          }
+          res.writeHead(200, {
+            'Content-Type': 'application/json'
+          });
+          return res.end(JSON.stringify(items));
+        }
       });
     });
   })).use('/public', Connect.static(__dirname + '/public')).listen(port);
