7574764ca7573915a86348438cc0024917b7f33f
Geoffrey Hing <geoff@terrorware.com>
2011-12-26 21:26:57 -0600

    Log a play to BGG by creating a cookie and add scaffolding for account signup.

--- boardgameqr.coffee
+++ boardgameqr.coffee
@@ -6,7 +6,15 @@ Parser = require 'xml2json'
 
 Mu.root = process.cwd() + '/templates'
 
+BGG_USERNAME = 'ghing'
+BGG_PASSWORD = 'dk10mb8mmj9jqdqr2kxc9cgvpqv5n2rcz'
+
 port = process.env.PORT || 8080
+
+buildPlayLogPostBody = (gameId, date) ->
+  month = date.getMonth() + 1
+  formattedDate = date.getFullYear() + '-' + month + '-' + date.getDate()
+  return "ajax=1&action=save&version=2&objecttype=thing&objectid=" + gameId + "&playid=&action=save&playdate=" + formattedDate + "&dateinput=" + formattedDate + "&YUIButton=&twitter=0&savetwitterpref=0&location=&quantity=1&length=&incomplete=0&nowinstats=0&comments="
 
 Connect.createServer()
 .use('/', Connect.query())
@@ -26,9 +34,23 @@ Connect.createServer()
         stream.on 'end', () -> res.end buffer
 
     app.get '/game/:id', (req, res, next) ->
-      geekURL = "http://boardgamegeek.com/boardgame/" + req.params.id + "/#collection"
-      res.writeHead 302, {"Location": geekURL}
-      res.end "Redirecting to Board Game Geek"
+      # Log a play on the Board Game Geek site.
+      geekURL = "http://boardgamegeek.com/geekplay.php"
+      j = Request.jar()
+      millisecondsInYr = 31556926000
+      today = new Date()
+      expires = new Date today.getTime() + millisecondsInYr
+      username_cookie = Request.cookie 'bggusername=' + BGG_USERNAME + ';path=/; expires=' + expires.toString()
+      j.add username_cookie
+      password_cookie = Request.cookie 'bggpassword=' + BGG_PASSWORD + '; path=/; expires=' + expires.toString()
+      j.add password_cookie
+      Request.post {url: geekURL, jar: j, body: buildPlayLogPostBody(req.params.id, today), headers: {'Content-Type': 'application/x-www-form-urlencoded' } }, (error, response, body) ->
+        if not error and response.statusCode == 200
+          console.log response
+          gameURL = "http://boardgamegeek.com/boardgame/" + req.params.id + "/"
+          res.writeHead 302, {"Location": gameURL}
+          res.end "Redirecting to Board Game Geek"
+
     app.get '/qr/game/:id', (req, res, next) ->
       gameURL = "http://" + req.headers.host + "/game/" + req.params.id + "/"
       res.writeHead 200, {'Content-Type': "text/html"}
@@ -45,6 +67,20 @@ Connect.createServer()
           stream.on 'data', (c) -> buffer += c
           stream.on 'end', () -> res.end buffer
 
+    app.get '/signup', (req, res, next) ->
+      res.writeHead 200, {"Content-Type": "text/html"}
+      Mu.compile 'signup.html', (err, parsed) ->
+        if err
+          console.log 'Error compiling template'
+
+        view = { title: 'Board Game QR' }
+
+        buffer = ''
+        stream = Mu.render parsed, view
+
+        stream.on 'data', (c) -> buffer += c
+        stream.on 'end', () -> res.end buffer
+
     app.get '/api/0.1/games/autocomplete', (req, res, next) ->
       Request 'http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=' + req.query.term, (error, response, body) ->
         if not error and response.statusCode == 200
@@ -55,6 +91,26 @@ Connect.createServer()
           
           res.writeHead 200, {'Content-Type': 'application/json'}
           res.end JSON.stringify(items)
+
+    app.post '/api/0.1/users', (req, res, next) ->
+      buffer = ''
+      req.on 'data', (c) -> buffer += c
+      req.on 'end', () ->
+        try
+          console.log buffer
+          userData = JSON.parse(buffer)
+          if userData.username? and userData.password? and userData.email? and userData.bggusername? and userData.bggpassword?
+            userResponse = {'username': userData.username, 'uri': '/api/0.1/users/' + userData.username}
+            res.writeHead 201, {'Content-Type': 'application/json'}
+            res.end JSON.stringify(userResponse)
+            # TODO: Write this to Postgres database
+          else
+            res.writeHead 400, {'Content-Type': 'application/json'}
+            res.end JSON.stringify({'error': 'Missing user attribute'})
+
+        catch error
+          res.writeHead 400, {'Content-Type': 'application/json'}
+          res.end JSON.stringify({'error': error.toString()})
 
 .use('/public', Connect.static __dirname + '/public')
 .listen port
--- boardgameqr.js
+++ boardgameqr.js
@@ -1,5 +1,5 @@
 (function() {
-  var Connect, Mu, Parser, QRCode, Request, port;
+  var BGG_PASSWORD, BGG_USERNAME, Connect, Mu, Parser, QRCode, Request, buildPlayLogPostBody, port;
 
   QRCode = require('qrcode');
 
@@ -13,7 +13,18 @@
 
   Mu.root = process.cwd() + '/templates';
 
+  BGG_USERNAME = 'ghing';
+
+  BGG_PASSWORD = 'dk10mb8mmj9jqdqr2kxc9cgvpqv5n2rcz';
+
   port = process.env.PORT || 8080;
+
+  buildPlayLogPostBody = function(gameId, date) {
+    var formattedDate, month;
+    month = date.getMonth() + 1;
+    formattedDate = date.getFullYear() + '-' + month + '-' + date.getDate();
+    return "ajax=1&action=save&version=2&objecttype=thing&objectid=" + gameId + "&playid=&action=save&playdate=" + formattedDate + "&dateinput=" + formattedDate + "&YUIButton=&twitter=0&savetwitterpref=0&location=&quantity=1&length=&incomplete=0&nowinstats=0&comments=";
+  };
 
   Connect.createServer().use('/', Connect.query()).use('/', Connect.router(function(app) {
     app.get('/', function(req, res, next) {
@@ -38,12 +49,34 @@
       });
     });
     app.get('/game/:id', function(req, res, next) {
-      var geekURL;
-      geekURL = "http://boardgamegeek.com/boardgame/" + req.params.id + "/#collection";
-      res.writeHead(302, {
-        "Location": geekURL
+      var expires, geekURL, j, millisecondsInYr, password_cookie, today, username_cookie;
+      geekURL = "http://boardgamegeek.com/geekplay.php";
+      j = Request.jar();
+      millisecondsInYr = 31556926000;
+      today = new Date();
+      expires = new Date(today.getTime() + millisecondsInYr);
+      username_cookie = Request.cookie('bggusername=' + BGG_USERNAME + ';path=/; expires=' + expires.toString());
+      j.add(username_cookie);
+      password_cookie = Request.cookie('bggpassword=' + BGG_PASSWORD + '; path=/; expires=' + expires.toString());
+      j.add(password_cookie);
+      return Request.post({
+        url: geekURL,
+        jar: j,
+        body: buildPlayLogPostBody(req.params.id, today),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }, function(error, response, body) {
+        var gameURL;
+        if (!error && response.statusCode === 200) {
+          console.log(response);
+          gameURL = "http://boardgamegeek.com/boardgame/" + req.params.id + "/";
+          res.writeHead(302, {
+            "Location": gameURL
+          });
+          return res.end("Redirecting to Board Game Geek");
+        }
       });
-      return res.end("Redirecting to Board Game Geek");
     });
     app.get('/qr/game/:id', function(req, res, next) {
       var gameURL;
@@ -71,7 +104,27 @@
         });
       });
     });
-    return app.get('/api/0.1/games/autocomplete', function(req, res, next) {
+    app.get('/signup', function(req, res, next) {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      return Mu.compile('signup.html', function(err, parsed) {
+        var buffer, stream, view;
+        if (err) console.log('Error compiling template');
+        view = {
+          title: 'Board Game QR'
+        };
+        buffer = '';
+        stream = Mu.render(parsed, view);
+        stream.on('data', function(c) {
+          return buffer += c;
+        });
+        return stream.on('end', function() {
+          return res.end(buffer);
+        });
+      });
+    });
+    app.get('/api/0.1/games/autocomplete', function(req, res, next) {
       return Request('http://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=' + req.query.term, function(error, response, body) {
         var item, items, js;
         if (!error && response.statusCode === 200) {
@@ -98,6 +151,44 @@
             'Content-Type': 'application/json'
           });
           return res.end(JSON.stringify(items));
+        }
+      });
+    });
+    return app.post('/api/0.1/users', function(req, res, next) {
+      var buffer;
+      buffer = '';
+      req.on('data', function(c) {
+        return buffer += c;
+      });
+      return req.on('end', function() {
+        var userData, userResponse;
+        try {
+          console.log(buffer);
+          userData = JSON.parse(buffer);
+          if ((userData.username != null) && (userData.password != null) && (userData.email != null) && (userData.bggusername != null) && (userData.bggpassword != null)) {
+            userResponse = {
+              'username': userData.username,
+              'uri': '/api/0.1/users/' + userData.username
+            };
+            res.writeHead(201, {
+              'Content-Type': 'application/json'
+            });
+            return res.end(JSON.stringify(userResponse));
+          } else {
+            res.writeHead(400, {
+              'Content-Type': 'application/json'
+            });
+            return res.end(JSON.stringify({
+              'error': 'Missing user attribute'
+            }));
+          }
+        } catch (error) {
+          res.writeHead(400, {
+            'Content-Type': 'application/json'
+          });
+          return res.end(JSON.stringify({
+            'error': error.toString()
+          }));
         }
       });
     });
